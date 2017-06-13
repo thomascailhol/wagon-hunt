cdf7560c2663245c0ebb945dfafd354755dfb641
Thomas Cailhol <thomascailhol@MacBook-Pro-de-Thomas-2.local>
2017-06-13 14:24:04 +0200

    add model product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @today = Date.today
+  end
+
+  def team
+    @members = ['Bob', 'Robin', 'Seb']
+  end
+
+  def join_us
+    render plain: "Send us an email!"
   end
 end
