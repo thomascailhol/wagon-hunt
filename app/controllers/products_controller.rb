450a6b228e13b8701ab650a220d5a65579de5b80
Thomas Cailhol <thomascailhol@MacBook-Pro-de-Thomas-2.local>
2017-06-13 15:05:47 +0200

    add resource rooting for product and CRUD actions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,19 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/team" => "pages#team"
   get "/contact" => "pages#join_us"
-  get "/products" => "products#index"
-  get "/products/:id" => "products#show" # montre details d'un pdt
+
+  resources :products # automatise routing CRUD + possibilité add contraitnes : , only: [:new, :create, :index, :show] automatise le routing CRUD
+
+  # resources :products équivalent à :
+  # # Read action routes (CRUD)
+  # get "/products" => "products#index"
+  # get "/products/:id" => "products#show" # montre details d'un pdt
+
+  # # Create action routes (CRUD)
+  # get "/products/new" => "products#new"  #naming conventionnel
+  # post "/products " => "products#create"
+
+  # # Update action routes (CRUD)
+  # get "/products/:id/edit" => "products#edit"  #naming conventionnel
+  # patch "/products/:id " => "products#update" # patch comme post mais update
+
+  # # Delete action routes (CRUD)
+  # delete "/products/:id/" => "products#destroy"  #naming conventionnel
 
 end
