7fc196983a0d6a4fad237f7e769046a3d4366bf9
Thomas Cailhol <thomascailhol@MacBook-Pro-de-Thomas-2.local>
2017-06-14 09:54:12 +0200

    front end refacto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,7 +35,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :url)
+    params.require(:product).permit(:name, :url, :tagline)
   end
   def find_product
     @product = Product.find(params[:id]) # get dans url id
