ee3b2a437004498d8533138a0b323b2583673687
Thomas Cailhol <thomascailhol@MacBook-Pro-de-Thomas-2.local>
2017-06-14 10:56:45 +0200

    add category to products and implemented filter by cat

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,11 @@ class ProductsController < ApplicationController
 
 
   def index
-    @products = Product.all
+    if params[:category]
+      @products = Product.where(category: params[:category])
+    else
+      @products = Product.all
+    end
   end
   def show
   end
@@ -35,7 +39,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :url, :tagline)
+    params.require(:product).permit(:name, :url, :tagline, :category)
   end
   def find_product
     @product = Product.find(params[:id]) # get dans url id
