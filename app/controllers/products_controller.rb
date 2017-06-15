8de32aacdffda854741136592342f815bf3e5edb
Thomas Cailhol <thomascailhol@MacBook-Pro-de-Thomas-2.local>
2017-06-15 10:34:22 +0200

    cloudinary + product/user photos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,7 +47,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :url, :tagline, :category)
+    params.require(:product).permit(:name, :url, :tagline, :category, :photo)
   end
   def find_product
     @product = Product.find(params[:id]) # get dans url id
